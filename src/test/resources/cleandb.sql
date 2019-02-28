cd8fc4c6e9661b0d252ec1af80d3b9fedb0a877d
ajpark30 <apark@madisoncollege.edu>
2019-02-27 20:03:43 -0600

    Added test database files.

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,2 +1,2 @@
-delete from user;
-INSERT INTO user VALUES (1,'Joe','Coyne','jcoyne','supersecret1','1964-04-01'),(2,'Fred','Hensen','fhensen','supersecret2','1988-05-08'),(3,'Barney','Curry','bcurry','supersecret3','1947-11-11'),(4,'Karen','Mack','kmack','supersecret4','1986-07-08'),(5,'Dianne','Klein','dklein','supersecret5','1991-09-22'),(6,'Dawn','Tillman','dtillman','supersecret6','1979-08-30');
+delete from test_mealprep;
+INSERT INTO test_mealprep VALUES ('Tester1','Test1','Incredible','Hulk', '1'),('Tester2','Test2','Peter','Parker', '2'),('Tester3','Test3','Captain','America', '3'),('Tester4','Test4','Black','Widow', '4'),('Tester5','Test5','Ant','Man', '5'),('Tester6','Test6','Captain','Marvel', '6');
